78301bdbe62a566f5c365ae30c058418bff3e826
KnifeForkAndShovel <76796056+KnifeForkAndShovel@users.noreply.github.com>
2026-05-24 20:59:45 +0100

    added test monitor
    monitor code in index as not sure how to link to monitor.js yet.

--- js/monitor.js
+++ js/monitor.js
@@ -1,1 +1,14 @@
 // monitor script will go here
+
+
+var element = document.getElementById('canvas')
+
+
+function onTouch(event) {
+    alert(event.type);
+}
+
+
+
+element.addEventListener('touch', onTouch);
+
